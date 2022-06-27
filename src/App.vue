9e4ff4063d7a8cafb28934526c3f3dbe6f4141e4
Alexandra M. Hall <hallstrategygroup@gmail.com>
2022-06-26 22:17:59 -0400

    added delete button + input field auto-reset

--- src/App.vue
+++ src/App.vue
@@ -23,6 +23,12 @@ const addTodo = () => {
     done: false,
     createdAt: new Date().getTime(),
   });
+  input_content.value = "";
+  input_category.value = "null";
+};
+
+const removeTodo = (todo) => {
+  todos.value = todos.value.filter((t) => t !== todo);
 };
 
 watch(
@@ -89,6 +95,27 @@ onMounted(() => {
         </div>
         <input type="submit" value="ADD TODO" />
       </form>
+    </section>
+    <section class="todo-list">
+      <h3>TODO LIST</h3>
+      <div class="list">
+        <div
+          v-for="todo in todos_asc"
+          :class="`todo-item ${todo.done && 'done'}`"
+        >
+          <label>
+            <input type="checkbox" v-model="todo.done" />
+            <span :class="`bubble ${todo.categpory}`"></span>
+          </label>
+          <div class="todo-content">
+            <input type="text" v-model="todo.content" />
+          </div>
+
+          <div class="actions">
+            <button class="delete" @click="removeTodo(todo)">Delete</button>
+          </div>
+        </div>
+      </div>
     </section>
   </main>
 </template>
